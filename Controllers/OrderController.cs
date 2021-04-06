59dac066ed010a10a6de74a00cd6879ce71945a6
Renan Melo <melorenan149@gmail.com>
2021-04-05 22:45:14 -0300

    My first Simple API

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn_API.Controllers
+{
+    [Route ("v1/order")]
+    public class OrderController: ControllerBase
+    {
+        [HttpGet]
+        [Route ("")]
+        public IActionResult Get()
+        {
+
+            return Ok("Presunto");
+        }
+        [HttpPost]
+        [Route("")]
+        public IActionResult Post()
+        {
+
+            return Ok("Mortadela");
+        }
+    }
+    
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn_API.Controllers
+{
+    [Route ("v1/user")]
+    public class UserController: ControllerBase
+    {
+        [HttpPut]
+        [Route("")]
+        public IActionResult UpdateAddress()
+        {
+            return Ok("Atualizado");
+        }
+        
+    }
+}
